3caa61ba5730a9c2db1511b9fc6b5cab50db4080
Dredd Villarba Domasian <Domasiandredd@gmail.com>
2025-01-23 17:41:29 +0800

    login
    gagawin palang

---
+++ login.php
@@ -0,0 +1,19 @@
+<?php
+    include("dbconnect.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    li
+    <title>Document</title>
+</head>
+<body>
+    <div>
+
+    </div>
+    
+</body>
+</html>
