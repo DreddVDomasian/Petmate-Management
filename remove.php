c88538a827b82c24b6a55e9eacbb73e8da04599e
Dredd Villarba Domasian <Domasiandredd@gmail.com>
2025-01-23 21:15:11 +0800

    oki na di na halata hahaha

--- remove.php
+++ remove.php
@@ -2,15 +2,14 @@
     include("dbconnect.php");
 
     if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['id'])) {
-        $id = intval($_POST['id']); // Sanitize the input
+        $id = intval($_POST['id']); 
 
-        // Prepare the delete query
+  
         $sql = "DELETE FROM petlists WHERE id = ?";
         $stmt = $con->prepare($sql);
         $stmt->bind_param("i", $id);
 
         if ($stmt->execute()) {
-            // Redirect back to the view page
             header("Location: view.php");
             exit;
         } else {
